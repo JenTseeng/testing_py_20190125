9ca7f6f574e9808e9b9beeed3b2c7aaa936bb657
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-25 15:55:56 -0800

    Updated test.py

--- tests.py
+++ tests.py
@@ -23,7 +23,10 @@ class PartyTests(unittest.TestCase):
         """Do users who haven't RSVPed see the correct view?"""
 
         # FIXME: Add a test to show we haven't RSVP'd yet
-        print("FIXME")
+        result = self.client.post("/rsvp")
+        self.assertIn(b"", result.data)
+
+        pass
 
     def test_rsvp(self):
         """Do RSVPed users see the correct view?"""
@@ -34,14 +37,13 @@ class PartyTests(unittest.TestCase):
                                   follow_redirects=True)
 
         # FIXME: check that once we log in we see party details--but not the form!
-        print("FIXME")
+        pass
 
     def test_rsvp_mel(self):
         """Can we keep Mel out?"""
 
         # FIXME: write a test that mel can't invite himself
         pass
-        print("FIXME")
 
 
 if __name__ == "__main__":
